b925000ef169aa81f6600d77c1dfc5c2be06d486
Gemuh Raharja <60685687+Sinichi21@users.noreply.github.com>
2025-12-23 22:33:54 +0800

    fix: name user teklegram notif

--- app/Jobs/SendTelegramAlertJob.php
+++ app/Jobs/SendTelegramAlertJob.php
@@ -38,7 +38,7 @@ class SendTelegramAlertJob implements ShouldQueue
                 'text' =>
                     "🚨 *CRITICAL ACTIVITY SIMOPKL*\n\n" .
                     "👤 User ID: {$log->user_id}\n" .
-                    "👥 Name: {$userName}\n" .
+                    "👥 Name: {$log->user->awardee->name}\n" .
                     "⚡ Action: {$log->action}\n" .
                     "📦 Module: {$log->module}\n" .
                     "📝 Desc: {$log->description}\n" .
