90c4617b64df500826ed612abdc2e985cd1333b4
Gemuh Raharja <60685687+Sinichi21@users.noreply.github.com>
2026-01-07 06:02:09 +0800

    fix: Document Index

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -32,4 +32,9 @@ class Document extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function documentType()
+    {
+        return $this->belongsTo(DocumentType::class);
+    }
 }
